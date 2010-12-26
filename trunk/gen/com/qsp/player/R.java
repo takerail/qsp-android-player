579f362fdd299f1238ee2517918fd43fb32603e6
nex_otaku@mail.ru <nex_otaku@mail.ru@93ef4c21-331d-246a-1384-3acb3253cf33>
2010-12-26 15:34:47 +0000

    * библиотека вынесена полностью в отдельный поток + добавлены диалоги InputBox, Menu

--- trunk/gen/com/qsp/player/R.java
+++ trunk/gen/com/qsp/player/R.java
@@ -8,50 +8,87 @@
 package com.qsp.player;
 
 public final class R {
+    public static final class anim {
+        public static final int update=0x7f040000;
+    }
     public static final class attr {
     }
+    public static final class color {
+        public static final int background1=0x7f050005;
+        public static final int background2=0x7f050006;
+        public static final int background3=0x7f050007;
+        public static final int foreground1=0x7f050003;
+        public static final int foreground2=0x7f050004;
+        public static final int title_background=0x7f050000;
+        public static final int title_separator=0x7f050008;
+        public static final int title_text=0x7f050001;
+        public static final int title_text_alt=0x7f050002;
+    }
+    public static final class dimen {
+        public static final int colorstrip_height=0x7f060000;
+        public static final int text_size_large=0x7f060004;
+        public static final int text_size_medium=0x7f060003;
+        public static final int text_size_small=0x7f060002;
+        public static final int title_height=0x7f060001;
+    }
     public static final class drawable {
-        public static final int ic_tab_inv=0x7f020000;
-        public static final int ic_tab_inv_selected=0x7f020001;
-        public static final int ic_tab_inv_unselected=0x7f020002;
-        public static final int ic_tab_main=0x7f020003;
-        public static final int ic_tab_main_selected=0x7f020004;
-        public static final int ic_tab_main_unselected=0x7f020005;
-        public static final int ic_tab_upd=0x7f020006;
-        public static final int ic_tab_upd_selected=0x7f020007;
-        public static final int ic_tab_upd_unselected=0x7f020008;
-        public static final int ic_tab_vars=0x7f020009;
-        public static final int ic_tab_vars_selected=0x7f02000a;
-        public static final int ic_tab_vars_unselected=0x7f02000b;
-        public static final int icon=0x7f02000c;
+        public static final int btn_bg_pressed=0x7f020000;
+        public static final int btn_bg_selected=0x7f020001;
+        public static final int ic_tab_inv=0x7f020002;
+        public static final int ic_tab_inv_alt=0x7f020003;
+        public static final int ic_tab_main=0x7f020004;
+        public static final int ic_tab_main_alt=0x7f020005;
+        public static final int ic_tab_vars=0x7f020006;
+        public static final int ic_tab_vars_alt=0x7f020007;
+        public static final int ic_title_home=0x7f020008;
+        public static final int ic_title_inv=0x7f020009;
+        public static final int ic_title_vars=0x7f02000a;
+        public static final int icon=0x7f02000b;
+        public static final int title_button=0x7f02000c;
         public static final int translucent_background=0x7f02000d;
     }
     public static final class id {
-        public static final int acts=0x7f060003;
-        public static final int imagebox=0x7f060000;
-        public static final int inv=0x7f060004;
-        public static final int main_desc=0x7f060002;
-        public static final int main_tab=0x7f060001;
-        public static final int vars_desc=0x7f060005;
+        public static final int acts=0x7f090006;
+        public static final int gestures=0x7f090003;
+        public static final int imagebox=0x7f090000;
+        public static final int inputbox_edit=0x7f090001;
+        public static final int inv=0x7f090007;
+        public static final int main_desc=0x7f090005;
+        public static final int main_tab=0x7f090004;
+        public static final int title_button_1=0x7f09000e;
+        public static final int title_button_2=0x7f090010;
+        public static final int title_home_button=0x7f09000a;
+        public static final int title_home_button_sep=0x7f09000b;
+        public static final int title_layout=0x7f090009;
+        public static final int title_sep_1=0x7f09000d;
+        public static final int title_sep_2=0x7f09000f;
+        public static final int title_text=0x7f09000c;
+        public static final int titlebar=0x7f090002;
+        public static final int vars_desc=0x7f090008;
     }
     public static final class layout {
         public static final int act_item=0x7f030000;
         public static final int image_box=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int obj_item=0x7f030003;
+        public static final int inputbox=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int obj_item=0x7f030004;
+        public static final int title=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f070001;
+        public static final int hello=0x7f070000;
     }
     public static final class style {
         /**  Base application theme is the default theme. 
          */
-        public static final int Theme=0x7f050000;
+        public static final int Theme=0x7f080000;
+        /**  Variation on the Light theme that turns off the title 
+         */
+        public static final int Theme_QSP=0x7f080002;
         /**  A theme that has a translucent background.  Here we explicitly specify
          that this theme is to inherit from the system's translucent theme,
          which sets up various attributes correctly. 
          */
-        public static final int Theme_Translucent=0x7f050001;
+        public static final int Theme_Translucent=0x7f080001;
     }
 }
