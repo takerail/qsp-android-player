17d3d952037605ae19fe4d9b160b4c778dc34fce
nex_otaku <nex_otaku@mail.ru>
2011-01-01 18:36:39 +0000

    + Сделано главное меню, теперь есть возможность переключаться между плеером и остальными приложениями, а также начать игру заново

--- gen/com/qsp/player/R.java
+++ gen/com/qsp/player/R.java
@@ -48,23 +48,30 @@ public final class R {
         public static final int translucent_background=0x7f02000d;
     }
     public static final class id {
-        public static final int acts=0x7f090006;
-        public static final int gestures=0x7f090003;
-        public static final int imagebox=0x7f090000;
-        public static final int inputbox_edit=0x7f090001;
-        public static final int inv=0x7f090007;
-        public static final int main_desc=0x7f090005;
-        public static final int main_tab=0x7f090004;
-        public static final int title_button_1=0x7f09000e;
-        public static final int title_button_2=0x7f090010;
-        public static final int title_home_button=0x7f09000a;
-        public static final int title_home_button_sep=0x7f09000b;
-        public static final int title_layout=0x7f090009;
-        public static final int title_sep_1=0x7f09000d;
-        public static final int title_sep_2=0x7f09000f;
-        public static final int title_text=0x7f09000c;
-        public static final int titlebar=0x7f090002;
-        public static final int vars_desc=0x7f090008;
+        public static final int acts=0x7f0a0006;
+        public static final int gestures=0x7f0a0003;
+        public static final int imagebox=0x7f0a0000;
+        public static final int inputbox_edit=0x7f0a0001;
+        public static final int inv=0x7f0a0007;
+        public static final int main_desc=0x7f0a0005;
+        public static final int main_tab=0x7f0a0004;
+        public static final int menu_about=0x7f0a0013;
+        public static final int menu_gamestock=0x7f0a0011;
+        public static final int menu_loadgame=0x7f0a0016;
+        public static final int menu_newgame=0x7f0a0015;
+        public static final int menu_options=0x7f0a0012;
+        public static final int menu_savegame=0x7f0a0017;
+        public static final int menugroup_running=0x7f0a0014;
+        public static final int title_button_1=0x7f0a000e;
+        public static final int title_button_2=0x7f0a0010;
+        public static final int title_home_button=0x7f0a000a;
+        public static final int title_home_button_sep=0x7f0a000b;
+        public static final int title_layout=0x7f0a0009;
+        public static final int title_sep_1=0x7f0a000d;
+        public static final int title_sep_2=0x7f0a000f;
+        public static final int title_text=0x7f0a000c;
+        public static final int titlebar=0x7f0a0002;
+        public static final int vars_desc=0x7f0a0008;
     }
     public static final class layout {
         public static final int act_item=0x7f030000;
@@ -73,6 +80,9 @@ public final class R {
         public static final int main=0x7f030003;
         public static final int obj_item=0x7f030004;
         public static final int title=0x7f030005;
+    }
+    public static final class menu {
+        public static final int main_menu=0x7f090000;
     }
     public static final class string {
         public static final int app_name=0x7f070001;
